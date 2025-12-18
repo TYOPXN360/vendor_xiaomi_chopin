8ef1c7b578bf16bb0ededabdb819911c359170da
AYIKxD <ayushmanbhowmik74@gmail.com>
2025-12-18 21:42:46 +0000

    chopin: Initial OSS vendor

---
+++ Android.mk
@@ -0,0 +1,27 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),chopin)
+
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,9ce2b34d40a45ed50ec1e6961b578316fcdbe786)
+$(call add-radio-file-sha1-checked,radio/cam_vpu1.img,4ee0abe5217614172909b6c677dc26430d0892df)
+$(call add-radio-file-sha1-checked,radio/cam_vpu2.img,b69d26cdeefaff471c03adbcc9d40e7fb6076133)
+$(call add-radio-file-sha1-checked,radio/cam_vpu3.img,364021548a2f0c67c0ce5ea1df8281d36bc60f83)
+$(call add-radio-file-sha1-checked,radio/dpm.img,d26b2fe431b0c2646a76c51654243b22786c5bff)
+$(call add-radio-file-sha1-checked,radio/dtbo.img,80a1194a15b9781c4c9c71bcf1d4ad3073d86f45)
+$(call add-radio-file-sha1-checked,radio/gz.img,f1f1b4b4355d0732d291bc5ea352793e670dd3ed)
+$(call add-radio-file-sha1-checked,radio/lk.img,9a6127c79192448c733c7a77cf09be1b1c10a077)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,2bfcdced296c83f0fd86e3c30289d153a39edb9a)
+$(call add-radio-file-sha1-checked,radio/md1img.img,88437b94dccda9e6f0ca6dc034e6104abd27dd61)
+$(call add-radio-file-sha1-checked,radio/mitee.img,6cba1b10bbb66d9f98ab7b7ea56e7309e47c0a41)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,61d373a9d6a3d6f9aa8580caf4fd1c123536befd)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,5e2e817ce1cbceff2cf4dbab42f4a43ee729ace3)
+$(call add-radio-file-sha1-checked,radio/scp.img,c53ed7f4f82967ab93ac889d2b0f7e95e529ea5d)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,6f1a5a3cb541fe6e8251238c09254c287db027ab)
+$(call add-radio-file-sha1-checked,radio/sspm.img,8efd7c51f360f1a32da9fac0af92cec583cecb40)
+$(call add-radio-file-sha1-checked,radio/tee.img,6e92e021c32937ff62eb5eebbfbba8c315f15860)
+
+endif
